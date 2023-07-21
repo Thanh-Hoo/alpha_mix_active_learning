7560e13a5046037df1e74de504c9223c55fa95c8
Thanh-Hoo <hohdt54@gmail.com>
2023-07-21 14:49:55 +0700

    update query code

--- query_strategies/alpha_mix_sampling.py
+++ query_strategies/alpha_mix_sampling.py
@@ -151,9 +151,9 @@ class AlphaMixSampling(Strategy):
 			
 		for i in range(self.model.clf.n_label):
 			emb = lb_embedding[Y == i]
-			print(f"emb: {emb}")
 			
 			if emb.size(0) == 0:
+				print("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
 				emb = lb_embedding
 			anchor_i = emb.mean(dim=0).view(1, -1).repeat(unlabeled_size, 1)
 
