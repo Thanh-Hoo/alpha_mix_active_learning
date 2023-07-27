7e3f25086fec5c2666121877bbc8a45546aee453
Thanh-Hoo <hohdt54@gmail.com>
2023-07-27 16:36:59 +0700

    update query code

--- query_strategies/alpha_mix_sampling.py
+++ query_strategies/alpha_mix_sampling.py
@@ -22,7 +22,7 @@ class AlphaMixSampling(Strategy):
 
 		ulb_probs, org_ulb_embedding = self.predict_prob_embed(self.X[idxs_unlabeled], self.Y[idxs_unlabeled])
 		probs_sorted, probs_sort_idxs = ulb_probs.sort(descending=True)
-		print(f"probs_sorted: {probs_sorted[0]}")
+		print(f"probs_sorted: {probs_sorted[0].size()}")
 		pred_1 = probs_sort_idxs[:, 0]
 
 		lb_probs, org_lb_embedding = self.predict_prob_embed(self.X[self.idxs_lb], self.Y[self.idxs_lb])
